255d40d06ace7cf5c62db1c0c8dd9aad5084995f
little-pan <pzp@maihesoft.com>
2019-03-19 22:55:40 +0800

    Create the SQLite server

---
+++ src/main/java/org/sqlite/server/SQLiteServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.sqlite.server;
+
+/**<p>
+ * The SQLite server based on the C/S architecture.
+ * </p>
+ * 
+ * @author little-pan
+ * @since 2019-03-19
+ *
+ */
+public class SQLiteServer {
+    
+    public static void main(String args[]) {
+        
+    }
+
+}
