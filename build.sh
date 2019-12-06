c76b6204127cb99880d2145ed0c6cd09817559ee
little-pan <pzp@maihesoft.com>
2019-12-06 18:27:24 +0800

    Add test jars in build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,11 @@ mvn dependency:copy-dependencies -DoutputDirectory=./lib
 
 export CLASSPATH=.:./target/classes:./target/test-classes\
 :./lib/logback-classic-1.1.7.jar:./lib/logback-core-1.1.7.jar:./lib/slf4j-api-1.7.21.jar\
-:./lib/sqlite-jdbc-3.28.0.jar
+:./lib/sqlite-jdbc-3.28.0.jar:./lib/antlr-2.7.6.jar:./lib/commons-collections-3.1.jar\
+:./lib/postgresql-42.2.5.jre7.jar:./lib/javassist-3.12.0.GA.jar:./lib/jta-1.1.jar\
+:./lib/hibernate-jpa-2.0-api-1.0.1.Final.jar:./lib/junit-4.12.jar:./lib/hibernate-core-3.6.10.Final.jar\
+:./lib/dom4j-1.6.1.jar:./lib/tomcat-juli-8.5.29.jar:./lib/hibernate-commons-annotations-3.2.0.Final.jar\
+:./lib/tomcat-jdbc-8.5.29.jar
 
 "$JAVA_HOME/bin/javac" -sourcepath src/main/java -d target/classes src/main/java/org/sqlite/server/*.java
 "$JAVA_HOME/bin/javac" -sourcepath src/test/java -d target/test-classes src/test/java/org/sqlite/*.java
